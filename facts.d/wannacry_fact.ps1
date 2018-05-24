508970d49a3c0d4feb08f8fbcd785cb212c60c8d
rnp16 <rnp16@zips.uakron.edu>
2018-05-24 15:03:11 +0000

    Delete wannacry_fact.ps1

--- facts.d/wannacry_fact.ps1
+++
@@ -1,7 +0,0 @@
-  $Policies = Get-ChildItem -Path "HKLM:\SOFTWARE\Altiris\Altiris Agent\Patch Management\Software Update\Policies" | Select-Object Name -ExpandProperty Name
- 
- if($Policies -eq $null) {$patched = "altirispatchpending=false"}
- else {$patched = "altirispatchpending=true"}
- 
-# Srv.sys check finished, print vulnerabiliy state
-Write-Host $patched
