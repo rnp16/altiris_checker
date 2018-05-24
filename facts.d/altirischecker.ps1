08cb389ae8d3f9404e1e17feec39ab145b74de3a
rnp16 <rnp16@zips.uakron.edu>
2018-05-24 15:02:49 +0000

    Create altirischecker.ps1

---
+++ facts.d/altirischecker.ps1
@@ -0,0 +1,5 @@
+ $Policies = Get-ChildItem -Path "HKLM:\SOFTWARE\Altiris\Altiris Agent\Patch Management\Software Update\Policies" | Select-Object Name -ExpandProperty Name  
+ if($Policies -eq $null) {$patched = "altirispatchpending=false"} 
+ else {$patched = "altirispatchpending=true"} 
+ # Srv.sys check finished, print vulnerabiliy state
+ Write-Host $patched
